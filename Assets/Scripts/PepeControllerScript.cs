c19bf7244e41e46a0b095d81e1ec0c7b1e2799c4
xxwikkixx <wikkii@msn.com>
2017-04-14 20:56:46 -0400

    added jump to the character

--- Assets/Scripts/PepeControllerScript.cs
+++ Assets/Scripts/PepeControllerScript.cs
@@ -8,12 +8,29 @@ public class PepeControllerScript : MonoBehaviour {
 
 	Animator anim;
 
+	//checking for ground presence
+	bool grounded = false;
+	public Transform groundCheck;
+	float groundRadius = 0.2f;
+	public LayerMask whatIsGround;
+	public float jumpForce = 700f;
+
+
+
 	// Use this for initialization
 	void Start () {
 		anim = GetComponent<Animator> ();
+		Rigidbody2D rigidbody2D = GetComponent<Rigidbody2D>();
 	}
 
 	void FixedUpdate(){
+
+		grounded = Physics2D.OverlapCircle (groundCheck.position, groundRadius, whatIsGround);
+		anim.SetBool ("Ground", grounded);
+		anim.SetFloat ("vSpeed", GetComponent<Rigidbody2D>().velocity.y);
+		//anim.SetFloat ("vSpeed", .velocity.y);
+
+
 		float move = Input.GetAxis ("Horizontal");
 		anim.SetFloat ("Speed", Mathf.Abs (move));
 
@@ -23,6 +40,13 @@ public class PepeControllerScript : MonoBehaviour {
 			Flip ();
 		else if (move < 0 && facingRight)
 			Flip ();
+	}
+
+	void Update(){
+		if (grounded && Input.GetKeyDown (KeyCode.Space)) {
+			anim.SetBool ("Ground", false);
+			GetComponent<Rigidbody2D>().AddForce (new Vector2 (0, jumpForce));
+		}
 	}
 
 	void Flip(){
